3face95b1319346fc1b2366d976099519b315d43
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-04 18:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class Employer {
 
   employees() { return store.customers.filter(customer => customer.employerId === this.id) }
   deliveries() { return store.deliveries.filter(delivery => delivery.customer().employerId === this.id) }
-  meals() { let meals = return this.deliveries().map(delivery => delivery.meal())
+  meals() { let meals = this.deliveries().map(delivery => delivery.meal())
     return meals.unique()
   }
 }
