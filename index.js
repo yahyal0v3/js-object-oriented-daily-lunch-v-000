f1ae10cfc72237a5d24ceda469ee14f86d5b628c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-04 17:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,11 +53,6 @@ class Employer {
     store.employers.push(this)
   }
 
-  employees() {
-   return store.customers.filter(customer => {
-     return customer.employerId === this.id
-   })
- }
-  
+  employees() { return store.customers.filter(customer => customer.employerId === this.id) }
   deliveries() { return this.employees().map(employee => employee.deliveries) }
 }
